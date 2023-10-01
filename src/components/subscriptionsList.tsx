440dd8bcbe3cf4f6b90c6b32f77d14e0fcb8cfb1
Huann Almeida <huannvictor@gmail.com>
2023-10-01 19:37:14 -0300

    implemented pending from useFormStatus

--- src/components/subscriptionsList.tsx
+++ src/components/subscriptionsList.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import { QueryResultRow } from "@vercel/postgres";
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
 import DeleteBtn from "./deleteBtn";
-
 
 export default function SubscriptionsList({rows}: {rows: QueryResultRow[] | undefined}) {  
   const f = new Intl.DateTimeFormat('pt-br', {
@@ -10,20 +10,26 @@ export default function SubscriptionsList({rows}: {rows: QueryResultRow[] | unde
     timeStyle: 'short',
   })
 
+  const { pending } = useFormStatus()
+
   return (
     <>
-    {rows && rows.map(subscriber => (
-      <tr key={subscriber.id} className="[&>*]:p-4">
-      <td>{subscriber.id}</td>
-      <td className="text-left">{subscriber.email}</td>
-      <td>{f.format(new Date(subscriber.created_at))}</td>
-      <td>
-        <DeleteBtn 
-          id={subscriber.id}
-        />
-      </td>
-    </tr>
-    ))}    
+    {
+      pending
+      ? <tr>carregando informações...</tr>
+      : rows && rows.map(subscriber => (
+        <tr key={subscriber.id} className="[&>*]:p-4">
+        <td>{subscriber.id}</td>
+        <td className="text-left">{subscriber.email}</td>
+        <td>{f.format(new Date(subscriber.created_at))}</td>
+        <td>
+          <DeleteBtn 
+            id={subscriber.id}
+          />
+          </td>
+        </tr>
+      ))
+    }    
     </>
   )
 }
